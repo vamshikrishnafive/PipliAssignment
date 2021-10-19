a73ee86aff39921025d0739607be69d36c1d45f8
vamshi krishna <53006565+vamshikrishnafive@users.noreply.github.com>
2021-10-20 01:25:18 +0530

    get details

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ const mongoose = require('mongoose')
 
 const workerCon = require("./controllers/upload-csv-worker")
 const Utilities = require('./controllers/upload-csv-logic');
-let storage = require('./controllers/multer-storage.js')
+const policy = require('./models/policy');
+let storage = require('./controllers/multer-storage.js');
+const { $where } = require('./models/policy');
+
 
 const app = express();
 app.use(express.json());
@@ -20,6 +23,16 @@ app.post('/upload-csv', upload.single('file'), async (req, res) => {
     result = await Utilities.uploadCsv(file);
     res.status(201).json(result);
 });
+app.get('/get-policy-details', async (req, res) => {
+    try {
+        let userName = req.query.userName;
+        let result = await policy.findOne({userName});
+        res.status(200).json(result)
+    } catch (error) {
+        console.error(error)
+        res.status(400).json({ Error: "NotFound" })
+    }
+})
 app.use("*", (req, res) => res.send({ error: "page not found" }));
 
 (async () => {
